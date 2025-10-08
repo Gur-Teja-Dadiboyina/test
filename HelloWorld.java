2be2cb83a4b3f825e07087e0c6a7f35535fcaec3
Gur-Teja-Dadiboyina <guruteja7743@gmail.com>
2025-10-08 16:41:48 +0530

    dfng

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Guru_Teja_Dadiboyina");
+        System.out.println("Teja_Dadiboyina");
+
+    }
+}
